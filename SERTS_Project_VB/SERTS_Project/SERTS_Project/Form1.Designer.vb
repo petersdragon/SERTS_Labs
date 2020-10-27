3bebbce19708901ce54609512435c074a62b823f
Peter Moyer <petersdragon@gmail.com>
2020-10-27 15:18:42 -0500

    Set up Project with Lab 9 Stuff so we can adapt it for the project.

---
+++ SERTS_Project_VB/SERTS_Project/SERTS_Project/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.sendFileButton = New System.Windows.Forms.Button()
+        Me.filesList = New System.Windows.Forms.ListBox()
+        Me.showFilesButton = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.SuspendLayout()
+        '
+        'sendFileButton
+        '
+        Me.sendFileButton.Location = New System.Drawing.Point(285, 66)
+        Me.sendFileButton.Name = "sendFileButton"
+        Me.sendFileButton.Size = New System.Drawing.Size(75, 23)
+        Me.sendFileButton.TabIndex = 5
+        Me.sendFileButton.Text = "Send"
+        Me.sendFileButton.UseVisualStyleBackColor = True
+        '
+        'filesList
+        '
+        Me.filesList.FormattingEnabled = True
+        Me.filesList.ItemHeight = 16
+        Me.filesList.Location = New System.Drawing.Point(189, 108)
+        Me.filesList.Name = "filesList"
+        Me.filesList.Size = New System.Drawing.Size(422, 276)
+        Me.filesList.TabIndex = 4
+        '
+        'showFilesButton
+        '
+        Me.showFilesButton.Location = New System.Drawing.Point(189, 66)
+        Me.showFilesButton.Name = "showFilesButton"
+        Me.showFilesButton.Size = New System.Drawing.Size(75, 23)
+        Me.showFilesButton.TabIndex = 3
+        Me.showFilesButton.Text = "Show Files"
+        Me.showFilesButton.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM5"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.sendFileButton)
+        Me.Controls.Add(Me.filesList)
+        Me.Controls.Add(Me.showFilesButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents sendFileButton As Button
+    Friend WithEvents filesList As ListBox
+    Friend WithEvents showFilesButton As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+End Class
---
+++ SERTS_Project_VB/SERTS_Project/SERTS_Project/Form1.vb
@@ -0,0 +1,94 @@
+﻿Imports System.Threading
+Public Class Form1
+    Delegate Sub ListBoxDelegate(ByVal command As Integer, ByVal myStr As String)
+    Dim ListBoxDel As New ListBoxDelegate(AddressOf ListBoxDelMethod)
+
+    Dim PC_Comm As New Thread(AddressOf PC_Comm_method)
+    Dim ShowFiles As Integer = 1
+    Dim StartFileList As Integer = 2
+    Dim EndFileList As Integer = 3
+
+    Dim ShowFilesStr As String = "1"
+    Dim StartFileListStr As String = "2"
+    Dim EndFileListStr As String = "3"
+
+    Dim b(1) As Byte
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Try to open the seial port 
+        Try
+            SerialPort1.Open()
+        Catch
+            Debug.WriteLine("Failed to open serial port")
+        End Try
+
+        ' Make this a background thread so it automatically
+        ' aborts when the main program stops.
+        PC_Comm.IsBackground = True
+        ' Set the thread priority to lowest
+        PC_Comm.Priority = ThreadPriority.Lowest
+        ' Start the thread
+        PC_Comm.Start()
+
+    End Sub
+    ' Delegate function that accesses the ListBox object
+    ' command = 2 - clear the contents of the ListBox
+    ' command = 3 - add the string to the ListBox
+    Public Sub ListBoxDelMethod(ByVal command As Integer, ByVal myStr As String)
+
+        If command = StartFileList Then
+            filesList.Items.Clear()
+        ElseIf command = EndFileList Then
+            filesList.Items.Add(myStr)
+        End If
+    End Sub
+    ' Thread that monitors the receive items on the serial port
+    Private Sub PC_Comm_method()
+        Dim str As String
+        While 1
+            If SerialPort1.IsOpen Then
+                Try
+                    str = SerialPort1.ReadLine() ' Wait for start string
+                Catch ex As Exception
+                End Try
+
+                'String.Compare return values:
+                ' Less than zero: strA precedes strB in the sort order.
+                'Zero" strA occurs in the same position as strB in the sort order.
+                'Greater than zero: strA follows strB in the sort order.
+                If Not String.Compare(str, StartFileListStr) Then
+                    ' Received a StartFileList response
+                    ' clear the list
+                    ' Use the delegate to access the ListBox
+                    filesList.Invoke(ListBoxDel, StartFileList, "")
+                    ' get next string
+                    Try
+                        str = SerialPort1.ReadLine() ' read file name
+                    Catch ex As Exception
+                    End Try
+                    While String.Compare(str, EndFileListStr)
+                        ' The read string is a file name and not the EndFileList
+                        filesList.Invoke(ListBoxDel, EndFileList, str)
+                        Try
+                            str = SerialPort1.ReadLine() ' read file name
+                        Catch ex As Exception
+                        End Try
+                    End While
+                    ' While loop ends when a 3 is received
+                End If
+            Else
+                Threading.Thread.Sleep(500)
+            End If
+        End While
+    End Sub
+    Private Sub filesList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles filesList.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub showFilesButton_Click(sender As Object, e As EventArgs) Handles showFilesButton.Click
+
+    End Sub
+
+    Private Sub sendFileButton_Click(sender As Object, e As EventArgs) Handles sendFileButton.Click
+
+    End Sub
+End Class
